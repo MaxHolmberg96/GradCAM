fd4b5a9bb6943c7404ee4e8798062804e4fe0341
Max <max.holmberg96@gmail.com>
2020-10-16 16:49:56 +0200

    Draws bounding boxes for each heatmap

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import tensorflow as tf
-import numpy as np
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
+
 from models.vgg16 import VGG16
 from models.gradcam import GradCAM
 from utils import *
@@ -8,31 +20,27 @@ from path import *
 
 images_list = get_files(ILSVRC2012VAL_PATH)
 boundingbox_list = get_files(ILSVRC2012VAL_BB_PATH)
+image_index = 513
+
 
 model = VGG16(weights="imagenet", classes=1000)
 model.summary()
-image = model.load_image(ILSVRC2012VAL_PATH + images_list[0])
+image = model.load_image(ILSVRC2012VAL_PATH + images_list[image_index])
 image = model.preprocess_image(image)
 image = tf.expand_dims(image, 0)
 preds = model.predict(image)
-print(preds.shape)
-model.summary()
-print(model.decode_predictions(preds, top=1))
+print(model.decode_predictions(preds, top=5))
 
 gradcam = GradCAM(model.model)
-heatmap = gradcam.get_heatmap(c=np.argmax(preds), image=image).numpy()
-threshold = (0.15 * np.max(heatmap))
-binarized_heatmap = np.zeros_like(heatmap)
-binarized_heatmap[heatmap > threshold] = 1
+heatmaps = []
+max_val = 0
+for predicted_class in get_top_class_indices(preds, top=5):
+    heatmaps.append(gradcam.get_heatmap(c=predicted_class, image=image).numpy())
+    max_val = max(max_val, np.max(np.uint8(heatmaps[-1] * 255)))
 
-original = model.load_image(ILSVRC2012VAL_PATH + images_list[0])
-original = tf.expand_dims(original, 0)
-original = draw_bounding_box_from_file(original, boundingbox_list[0])
-draw_bounding_box_from_heatmap(image, heatmap)
-gradcam.show(binarized_heatmap, original)
-#import matplotlib.pyplot as plt
-#plt.imshow(original.numpy()[0, ...] / 255)
-#plt.show()
-#gradcam.show(heatmap, image)
+for heatmap in heatmaps:
+    image = draw_bounding_box_from_heatmap(image, heatmap, max_val)
+
+show_image(image)
 
 
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import xml.etree.ElementTree as ET
 import cv2
+import matplotlib.pyplot as plt
 
 
 def get_files(file_path):
@@ -33,8 +34,36 @@ def draw_bounding_box_from_file(image, file_path):
     return draw_bounding_box(image, xmin, ymin, xmax, ymax)
 
 
-def draw_bounding_box_from_heatmap(image, heatmap):
+def draw_bounding_box_from_heatmap(image, heatmap, max_val):
     heatmap = cv2.resize(heatmap, (image.shape[2], image.shape[1]), cv2.INTER_LINEAR)
-    threshhold = 0.15 * np.max(heatmap)
-    thresh = cv2.threshold(heatmap, threshhold, np.max(heatmap), cv2.THRESH_BINARY)
+    heatmap = np.uint8(heatmap * 255)
+    threshold = 0.15 * max_val
+    thresh = cv2.threshold(heatmap, threshold, max_val, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
     cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    #img = np.uint8(image)
+    #cv2.drawContours(img[0], cnts, -1, (0, 255, 0), 3)
+    #cv2.imshow('Contours', img[0])
+    max_contour = -1
+    max_area = -1
+    for c in cnts:
+        area = cv2.contourArea(c)
+        if area > max_area:
+            max_area = area
+            max_contour = c
+
+    if max_area != -1:
+        xmin, ymin, w, h = cv2.boundingRect(max_contour)
+        xmax = xmin + w
+        ymax = ymin + h
+        return draw_bounding_box(image, xmin, ymin, xmax, ymax)
+    return image
+
+
+def get_top_class_indices(preds, top=5):
+    return np.argsort(-preds)[0][:top]
+
+
+def show_image(image):
+    plt.imshow(image.numpy()[0, ...] / 255)
+    plt.show()
